c078cd7959b66bd989ec4662fb324766040faf04
rcedermalm <rebecca@cedermalm.se>
2018-12-04 11:37:48 +0100

    Opened up the ShaderProgram class to also include compute shaders

--- include/ShaderProgram.hpp
+++ include/ShaderProgram.hpp
@@ -10,15 +10,10 @@
 #include <iostream>
 
 /// Adapted from http://stackoverflow.com/questions/2795044/easy-framework-for-opengl-shaders-in-c-c
+/// Class has been changed to also include compute shader by Rebecca Cedermalm (changes in hierarchy as well)
 
-class ShaderProgram {
+class BaseShaderProgram {
 public:
-    /// Constructs a GLSL shader program with V/TC/TE/G/F-shaders located in the specified files
-    ShaderProgram(std::string vertex_shader_filename = "",
-                  std::string tessellation_control_shader_filename = "",
-                  std::string tessellation_eval_shader_filename = "",
-                  std::string geometry_shader_filename = "",
-                  std::string fragment_shader_filename = "");
 
     /// Get the GLuint corresponding to the OpenGL shader program
     inline operator GLuint() {
@@ -30,21 +25,40 @@ public:
         glUseProgram(prog);
     }
 
+protected:
+
+    /// Constructs a GLSL shader program with V/TC/TE/G/F-shaders located in the specified files
+    BaseShaderProgram(){};
+
+
+
     static const std::string getShaderType(GLuint type);
 
     GLint MV_Loc, P_Loc, lDir_Loc, camPos_Loc = -1;
     GLint tmpTex = -1;
 
-    ~ShaderProgram();
+    ~BaseShaderProgram();
 
-protected:
     GLuint AttachShader(GLuint shaderType, std::string source);
 
     void ConfigureShaderProgram();
 
-private:
     std::vector<GLuint> shader_programs_;
     GLuint prog;
 
     GLuint compile(GLuint type, GLchar const *source);
+};
+
+class ShaderProgram : public BaseShaderProgram {
+public:
+    ShaderProgram(std::string vertex_shader_filename = "",
+            std::string tessellation_control_shader_filename = "",
+            std::string tessellation_eval_shader_filename = "",
+            std::string geometry_shader_filename = "",
+            std::string fragment_shader_filename = "");
+};
+
+class ComputeShader : public BaseShaderProgram {
+public:
+    ComputeShader(std::string compute_shader_filename = "");
 };
--- src/ShaderProgram.cpp
+++ src/ShaderProgram.cpp
@@ -5,11 +5,98 @@
 #include <cstdlib>
 
 
-ShaderProgram::ShaderProgram(std::string vertex_shader_filename, std::string tessellation_control_shader_filename,
-                             std::string tessellation_eval_shader_filename, std::string geometry_shader_filename,
-                             std::string fragment_shader_filename) {
+BaseShaderProgram::~BaseShaderProgram() {
+    for (GLuint shader_program : shader_programs_) {
+        glDeleteShader(shader_program);
+    }
+    glDeleteProgram(prog);
+}
 
+GLuint BaseShaderProgram::AttachShader(GLuint shaderType, std::string source) {
+    GLuint sh = compile(shaderType, source.c_str());
+    shader_programs_.push_back(sh);
 
+    std::cout << "Attached shader of type: '" << getShaderType(shaderType) << "'\n";
+
+    return sh;
+}
+
+void BaseShaderProgram::ConfigureShaderProgram() {
+    prog = glCreateProgram();
+
+    for (GLuint shader_program : shader_programs_) {
+        glAttachShader(prog, shader_program);
+    }
+
+    glLinkProgram(prog);
+
+    GLint isLinked;
+    glGetProgramiv(prog, GL_LINK_STATUS, (int *) &isLinked);
+
+    if (isLinked == GL_FALSE) {
+        GLint length;
+        glGetShaderiv(prog, GL_INFO_LOG_LENGTH, &length);
+        std::string log(length, ' ');
+        glGetShaderInfoLog(prog, length, &length, &log[0]);
+        std::cerr << "Failed to link shaderprogram : " << std::endl
+        << log << std::endl;
+        exit(EXIT_FAILURE);
+    } else {
+        std::cout << "Shader linking complete!\n";
+    }
+}
+
+const std::string BaseShaderProgram::getShaderType(GLuint type) {
+    std::string name;
+    switch (type) {
+        case GL_VERTEX_SHADER:
+            name = "Vertex Shader";
+            break;
+        case GL_TESS_CONTROL_SHADER:
+            name = "Tessellation Control Shader";
+            break;
+        case GL_TESS_EVALUATION_SHADER:
+            name = "Tessellation Evaluation Shader";
+            break;
+        case GL_GEOMETRY_SHADER:
+            name = "Geometry Shader";
+            break;
+        case GL_FRAGMENT_SHADER:
+            name = "Fragment Shader";
+            break;
+        case GL_COMPUTE_SHADER:
+            name = "Compute Shader";
+        default:
+            name = "Unknown Shader type";
+            break;
+    }
+    return name;
+}
+
+GLuint BaseShaderProgram::compile(GLuint type, GLchar const *source) {
+    GLuint shader = glCreateShader(type);
+    glShaderSource(shader, 1, &source, NULL);
+    glCompileShader(shader);
+    GLint compiled = 0;
+    glGetShaderiv(shader, GL_COMPILE_STATUS, &compiled);
+    if (compiled == GL_FALSE) {
+        GLint logSize = 0;
+        glGetShaderiv(shader, GL_INFO_LOG_LENGTH, &logSize);
+        std::string log(logSize, ' ');
+        glGetShaderInfoLog(shader, logSize, &logSize, &log[0]);
+        std::cerr << "Failed to compile shadertype: " << getShaderType(type) << std::endl
+        << log << std::endl;
+        glDeleteShader(shader); // Don't leak the shader.
+        exit(EXIT_FAILURE);
+    }
+    return shader;
+}
+
+ShaderProgram::ShaderProgram(std::string vertex_shader_filename,
+        std::string tessellation_control_shader_filename,
+        std::string tessellation_eval_shader_filename,
+        std::string geometry_shader_filename,
+        std::string fragment_shader_filename){
 
     if (vertex_shader_filename != "") {
         auto v_source = FileReader::ReadFromFile(vertex_shader_filename);
@@ -41,88 +128,18 @@ ShaderProgram::ShaderProgram(std::string vertex_shader_filename, std::string tes
     }
 }
 
-ShaderProgram::~ShaderProgram() {
+
+ComputeShader::ComputeShader(std::string compute_shader_filename){
+    if (compute_shader_filename != "") {
+        auto v_source = FileReader::ReadFromFile(compute_shader_filename);
+        AttachShader(GL_COMPUTE_SHADER, v_source);
+    }
+
+    //Link shader
+    ConfigureShaderProgram();
+
+    //Detach shader after successful linking
     for (GLuint shader_program : shader_programs_) {
-        glDeleteShader(shader_program);
+        glDetachShader(prog, shader_program);
     }
-    glDeleteProgram(prog);
-
-}
-
-GLuint ShaderProgram::AttachShader(GLuint shaderType, std::string source) {
-    GLuint sh = compile(shaderType, source.c_str());
-    shader_programs_.push_back(sh);
-
-    std::cout << "Attached shader of type: '" << getShaderType(shaderType) << "'\n";
-
-    return sh;
-}
-
-void ShaderProgram::ConfigureShaderProgram() {
-    prog = glCreateProgram();
-
-    for (GLuint shader_program : shader_programs_) {
-        glAttachShader(prog, shader_program);
-    }
-
-    glLinkProgram(prog);
-
-    GLint isLinked;
-    glGetProgramiv(prog, GL_LINK_STATUS, (int *) &isLinked);
-
-    if (isLinked == GL_FALSE) {
-        GLint length;
-        glGetShaderiv(prog, GL_INFO_LOG_LENGTH, &length);
-        std::string log(length, ' ');
-        glGetShaderInfoLog(prog, length, &length, &log[0]);
-        std::cerr << "Failed to link shaderprogram : " << std::endl
-        << log << std::endl;
-        exit(EXIT_FAILURE);
-    } else {
-        std::cout << "Shader linking complete!\n";
-    }
-}
-
-const std::string ShaderProgram::getShaderType(GLuint type) {
-    std::string name;
-    switch (type) {
-        case GL_VERTEX_SHADER:
-            name = "Vertex Shader";
-            break;
-        case GL_TESS_CONTROL_SHADER:
-            name = "Tessellation Control Shader";
-            break;
-        case GL_TESS_EVALUATION_SHADER:
-            name = "Tessellation Evaluation Shader";
-            break;
-        case GL_GEOMETRY_SHADER:
-            name = "Geometry Shader";
-            break;
-        case GL_FRAGMENT_SHADER:
-            name = "Fragment Shader";
-            break;
-        default:
-            name = "Unknown Shader type";
-            break;
-    }
-    return name;
-}
-
-GLuint ShaderProgram::compile(GLuint type, GLchar const *source) {
-    GLuint shader = glCreateShader(type);
-    glShaderSource(shader, 1, &source, NULL);
-    glCompileShader(shader);
-    GLint compiled = 0;
-    glGetShaderiv(shader, GL_COMPILE_STATUS, &compiled);
-    if (compiled == GL_FALSE) {
-        GLint logSize = 0;
-        glGetShaderiv(shader, GL_INFO_LOG_LENGTH, &logSize);
-        std::string log(logSize, ' ');
-        glGetShaderInfoLog(shader, logSize, &logSize, &log[0]);
-        std::cerr << "Failed to compile shadertype: " << getShaderType(type) << std::endl
-        << log << std::endl;
-        glDeleteShader(shader); // Don't leak the shader.
-        exit(EXIT_FAILURE);
-    }
-    return shader;
 }
